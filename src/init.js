3b2768e14b32505ad98aa3f61bc189003feb5f09
Aleksandra <alex.shalimova@gmail.com>
2020-08-27 16:42:52 +0200

    Initial look of the quiz Added starting page, upload of the first question Closes gh-3

--- src/init.js
+++ src/init.js
@@ -8,6 +8,13 @@ window.onload = () => {
   log.push({
     state: deepClone(state)
   });
+
+   // Show the setup
+   document.getElementById("explanation_div").style.display = "block";
+   
+   // Hide the game
+   document.getElementById("play_quiz_div").style.display = "none";
+
   // this log will always be up to date because of reference
   console.log(log);
 };
